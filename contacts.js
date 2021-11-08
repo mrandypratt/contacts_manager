a4ca77a913dc57a2901d24cad532d327141480e6
MrAndyPratt <mrandypratt@gmail.com>
2021-11-07 22:01:01 -0600

    Full Validation of Input and Inputs saved in case of erroroneous input

--- contacts.js
+++ contacts.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const app = express();
+const maxNameLength = 25;
 
 let contactData = [
   {
@@ -65,15 +66,22 @@ app.get("/contacts/new", (req, res) => {
 app.post("/contacts/new",
   (req, res, next) => {
     res.locals.errorMessages = [];
-    
+
     next();
   },
 
   (req, res, next) => {
+    for (let inputItem in req.body) {
+      req.body[inputItem] = req.body[inputItem].trim();
+    }
+    
+    next();
+  },
+  (req, res, next) => {
     if (req.body.firstName.length === 0) {
       res.locals.errorMessages.push("First name is required.");
     }
-
+    
     next();
   },
   (req, res, next) => {
@@ -91,9 +99,50 @@ app.post("/contacts/new",
     next();
   },
   (req, res, next) => {
+    if ((req.body.firstName.length > maxNameLength) || (req.body.lastName.length > maxNameLength)) {
+      res.locals.errorMessages.push("Names must be 25 characters or less.");
+    }
+    
+    next();
+  },
+  (req, res, next) => {
+    if ((req.body.firstName.match(/[^A-Za-z]/g)) || (req.body.lastName.match(/[^A-Za-z]/g))) {
+      res.locals.errorMessages.push("Names can only contain Alphabetical Characters.");
+    }
+    
+    next();
+  },
+  (req, res, next) => {
+    contactData.forEach(storedContact => {
+      if (storedContact.firstName === req.body.firstName && storedContact.lastName === req.body.lastName) {
+        res.locals.errorMessages.push("Contact already exists");
+      }
+    });
+
+    next();
+  },
+  (req, res, next) => {
+    if (req.body.phoneNumber.match(/[^0-9]/g)) {
+      res.locals.errorMessages.push("Phone Number may only contain digits.");
+    }
+
+    next();
+  },
+  (req, res, next) => {
+    if (req.body.phoneNumber.length !== 10) {
+      res.locals.errorMessages.push("Phone Number must only contain 10 characters.");
+    }
+
+    next();
+  },
+
+  (req, res, next) => {
     if (res.locals.errorMessages.length > 0) {
       res.render("create_new_contact", {
         errorMessages: res.locals.errorMessages,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        phoneNumber: req.body.phoneNumber,
       });
     } else {
       next();
